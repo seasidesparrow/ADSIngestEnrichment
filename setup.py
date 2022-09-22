9f85ab6c4128ba8c3e52990210cf91608ef5f87a
Matthew Templeton <matthew.templeton@cfa.harvard.edu>
2022-09-22 13:30:13 -0400

    feat: setting up bibcode creation, packaging  	modified:   README.md  	new file:   adsenrich/__init__.py  	new file:   adsenrich/bibcode_data/issn2bibstem.txt  	new file:   adsenrich/bibcode_data/name2bibstem.txt  	new file:   adsenrich/bibcodes.py  	new file:   adsenrich/data.py  	new file:   adsenrich/utils.py  	new file:   requirements.txt  	new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,50 @@
+import os
+from subprocess import Popen, PIPE
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    long_description = ""
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+
+def get_git_version(default="v0.0.1"):
+    try:
+        p = Popen(['git', 'describe', '--tags'], stdout=PIPE, stderr=PIPE)
+        p.stderr.close()
+        line = p.stdout.readlines()[0]
+        line = line.strip()
+        return line.decode()
+    except:
+        return default
+
+setup(
+    name='adsenrich',
+    version=get_git_version(default="v0.0.1"),
+    classifiers=['Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.8'],
+    url='https://github.com/adsabs/ADSIngestEnrichment',
+    license='Gnu GPL',
+    author="NASA/SAO ADS",
+    description='ADS Ingest Enrichment',
+    long_description=long_description,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=required,
+    #entry_points={
+    #      'kombu.serializers': [
+    #          'adsmsg = adsputils.serializer:register_args'
+    #      ]
+    #  }
+  )
